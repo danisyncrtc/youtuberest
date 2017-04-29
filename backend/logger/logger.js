8042810c057379ce92a0ba9e07f96ad4111183af
Daniel Puebla Martin <dani@syncrtc.com>
2017-04-29 11:15:28 +0200

    prueba2

--- backend/logger/logger.js
+++ backend/logger/logger.js
@@ -1,8 +1,21 @@
 'use strict';
 
 const bunyan = require('bunyan');
+const BunyanStackDriver = require('bunyan-stackdriver')
 const uuidV4 = require('uuid/v4');
 const loggerServ = (process.env.LOGGER_SERV || 'backnd');
+
+
+const stackDriver = new BunyanStackDriver({
+  // The path to your key file:
+  keyFilename: '../prueba-33960b2c3da9.json',
+  // Or the contents of the key file:
+  credentials: require('../prueba-33960b2c3da9.json'),
+  logName: 'logname',
+  projectId: 'prueba-164211',
+  writeInterval: 500, // ms
+}, function errorCallback(err) { console.log(err); });
+
 
 function idSerializer(request) {
   const req = request[0];
@@ -26,6 +39,14 @@ function idSerializer(request) {
   };
 }
 
-const logger = bunyan.createLogger({ name: loggerServ, serializers: { request: idSerializer, err: bunyan.stdSerializers.err }, src: true });
+const logger = bunyan.createLogger({
+  name: loggerServ,
+  serializers: { request: idSerializer, err: bunyan.stdSerializers.err },
+  src: true,
+  streams: [{
+    type: 'raw', // faster; makes Bunyan send objects instead of stringifying messages
+    stream: stackDriver
+  }]
+});
 
 module.exports = logger;
